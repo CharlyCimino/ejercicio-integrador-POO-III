0716411bc0f8aa454ce62002e26a2dbf5421a215
Your Name <you@example.com>
2022-04-01 08:41:46 -0300

    Terminado

---
+++ src/baraja/cartas/Carta.java
@@ -0,0 +1,25 @@
+package baraja.cartas;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public abstract class Carta {
+
+    private boolean tapada;
+
+    public Carta() {
+        // Por defecto, 'tapada' es false
+    }
+
+    private void darVuelta() {
+
+    }
+
+    public void mostrar() {
+
+    }
+
+}
---
+++ src/baraja/cartas/CartaNumeral.java
@@ -0,0 +1,20 @@
+package baraja.cartas;
+
+import baraja.Palo;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public class CartaNumeral extends CartaConPalo {
+
+    private int numero;
+
+    public CartaNumeral(int numero, Palo palo) {
+        super(palo);
+        this.numero = numero;
+    }
+
+}
